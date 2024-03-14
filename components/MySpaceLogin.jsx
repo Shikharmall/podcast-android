0c1fea1933d45b968ebf0beb58dc578356e95af1
Shikhar Mall <shmall.21.2020@gmail.com>
2024-03-14 23:16:08 +0530

    myspacelogin component

---
+++ components/MySpaceLogin.jsx
@@ -0,0 +1,132 @@
+import {Image, SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import React, {Component} from 'react';
+import {Path, Svg} from 'react-native-svg';
+import LinearGradient from 'react-native-linear-gradient';
+
+export default class MySpaceLogin extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          backgroundColor: '#0f1014',
+          flex: 1,
+        }}>
+        <SafeAreaView style={{flex: 1}}>
+          <View
+            style={{
+              padding: 5,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <Image
+              source={require('../image/thesaruslogo.jpg')}
+              style={{
+                height: 40,
+                width: 40,
+                borderRadius: 5,
+              }}
+            />
+
+            <TouchableOpacity style={{flexDirection: 'row'}}>
+              <Svg
+                width="18px"
+                height="18px"
+                viewBox="0 0 24 24"
+                fill="white"
+                xmlns="http://www.w3.org/2000/svg">
+                <Path
+                  fillRule="evenodd"
+                  d="M20.873 18.68l-2.217 2.217-2.838-.79-.811.334L13.55 23h-3.135l-1.449-2.567-.81-.34-2.838.781-2.216-2.216.79-2.838-.334-.811L1 13.55v-3.135l2.568-1.449.34-.81-.781-2.838 2.215-2.215 2.838.791.81-.334L10.446 1h3.136l1.449 2.568.81.34 2.837-.781 2.22 2.215-.792 2.839.335.81L23 10.447v3.136l-2.567 1.448-.34.813.78 2.837zm-2.892-2.972l.872-2.082L21 12.414v-.804l-2.147-1.22-.859-2.083.662-2.375-.569-.568-2.383.655-2.08-.872L12.413 3h-.804l-1.22 2.147-2.083.859-2.376-.663-.566.566.655 2.383-.872 2.08L3 11.583v.805l2.145 1.222.859 2.083-.662 2.376.567.567 2.383-.655 2.08.872 1.21 2.146h.805l1.222-2.145 2.083-.859 2.376.662.568-.568-.655-2.381zM12 16a4 4 0 110-8 4 4 0 010 8zm0-2a2 2 0 100-4 2 2 0 000 4z"
+                />
+              </Svg>
+              <Text
+                style={{
+                  color: 'white',
+                  fontWeight: 'bold',
+                  fontSize: 12,
+                  marginLeft: 3,
+                }}>
+                Help & Settings
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              padding: 5,
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            {/*<Image
+              source={require('../image/logoApp.png')}
+              style={{
+                height: 400,
+                width: '100%',
+                //margin: 5
+                //borderRadius: 5,
+              }}
+            />*/}
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 20,
+                fontWeight: 'bold',
+                marginVertical: 15,
+              }}>
+              Login to The SARUS
+            </Text>
+            <Text style={{color: 'gray', fontSize: 12, alignSelf: 'center'}}>
+              Start watching the latest and entertaining podcasts.
+            </Text>
+            {/*<TouchableOpacity
+              style={{
+                backgroundColor: '#0a55d4',
+                paddingHorizontal: 80,
+                paddingVertical: 18,
+                borderRadius: 10,
+                marginVertical: 30
+              }}>
+              <Text style={{color: 'white', fontSize: 15 , fontWeight: 'bold'}}>Log In</Text>
+            </TouchableOpacity>*/}
+
+            <TouchableOpacity
+              style={{
+                borderRadius: 10,
+                marginVertical: 25,
+                overflow: 'hidden', // Ensures the gradient doesn't overflow the TouchableOpacity boundaries
+              }}>
+              <LinearGradient
+                colors={['#0a55d4', '#174696']} // Specify your two color shades here
+                start={{x: 0, y: 0}} // Gradient start point
+                end={{x: 1, y: 0}} // Gradient end point
+                style={{
+                  paddingHorizontal: 80,
+                  paddingVertical: 18,
+                  borderRadius: 10,
+                }}>
+                <Text
+                  style={{color: 'white', fontSize: 15, fontWeight: 'bold'}}>
+                  Log In
+                </Text>
+              </LinearGradient>
+            </TouchableOpacity>
+
+            <Text
+              style={{
+                color: 'gray',
+                fontSize: 12,
+                margin: 5,
+                marginVertical: 20,
+              }}>
+              Having trouble logging in?{' '}
+              <Text style={{color: '#0a55d4', fontWeight: 'bold'}}>
+                Get Help
+              </Text>
+            </Text>
+          </View>
+        </SafeAreaView>
+      </View>
+    );
+  }
+}
--- screens/MySpace.jsx
+++ screens/MySpace.jsx
@@ -7,187 +7,205 @@ import {
   Image,
   TouchableOpacity,
 } from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Path, Svg} from 'react-native-svg';
 import MySpaceTab from '../components/MySpaceTab';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import MySpaceLogin from '../components/MySpaceLogin';
 
 export default function MySpace({navigation}) {
   const screenWidth = Dimensions.get('window').width;
   //const screenHeight = Dimensions.get('window').height;
 
   const [isShow, setIsShow] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
   const togglePopUp = () => {
     setIsShow(!isShow);
   };
 
+  const handleLogin = async () => {
+    //console.log('Hello');
+    const userToken = await AsyncStorage.getItem('userToken');
+    if (userToken) {
+      setIsLogin(true);
+    }
+    console.log(userToken);
+  };
+
+  useEffect(() => {
+    handleLogin();
+  }, []);
+
   return (
-    <View
-      style={{
-        backgroundColor: '#0f1014',
-        flex: 1,
-      }}>
-      <SafeAreaView
-        style={{
-          position: 'relative',
-        }}>
-        {/* header */}
-        <View style={{position: 'relative'}}>
-          <TouchableOpacity
-            //activeOpacity={1}
-            onPress={() => {
-              navigation.navigate('CoverImageUpdate');
-            }}>
-            <View
-              style={{
-                backgroundColor: 'gray',
-                height: 150,
-                width: screenWidth,
-              }}></View>
-          </TouchableOpacity>
-          <View
-            style={{
-              backgroundColor: '#0f1014',
-              height: 80,
-              width: 80,
-              borderRadius: 50,
-              position: 'absolute',
-              bottom: -40,
-              left: 30,
-              padding: 3,
-            }}>
-            <TouchableOpacity
-              //activeOpacity={1}
-              onPress={() => {
-                navigation.navigate('ProfileImageUpdate');
-              }}>
-              <Image
-                source={require('../image/user.png')}
-                style={{width: '100%', height: '100%'}}
-              />
-            </TouchableOpacity>
-          </View>
-
-          <TouchableOpacity
-            onPress={togglePopUp}
-            activeOpacity={1}
-            style={{
-              //backgroundColor: '#0f1014',
-              //backgroundColor: 'rgba(255, 255, 255, 0.1)',
-              backgroundColor: 'rgba(255, 255, 255, 0.2)',
-              position: 'absolute',
-              top: 0,
-              right: 0,
-              padding: 7,
-              borderRadius: 50,
-              margin: 2,
-            }}>
-            <Svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20px"
-              height="20px"
-              fill="#ffffff"
-              viewBox="0 0 52 52">
-              <Path d="M20 44c0-3.3 2.7-6 6-6s6 2.7 6 6-2.7 6-6 6-6-2.7-6-6zm0-18c0-3.3 2.7-6 6-6s6 2.7 6 6-2.7 6-6 6-6-2.7-6-6zm0-18c0-3.3 2.7-6 6-6s6 2.7 6 6-2.7 6-6 6-6-2.7-6-6z" />
-            </Svg>
-          </TouchableOpacity>
-
-          {isShow ? (
-            <View
-              style={{
-                backgroundColor: '#0f1014',
-                position: 'absolute',
-                top: 40,
-                right: 25,
-                padding: 7,
-                width: 200,
-                height: 100,
-                borderRadius: 10,
-              }}></View>
-          ) : null}
-        </View>
-
+    <>
+      {isLogin ? (
         <View
           style={{
-            //backgroundColor: 'red',
-            padding: 10,
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
+            backgroundColor: '#0f1014',
+            flex: 1,
           }}>
-          <Text
+          <SafeAreaView
             style={{
-              padding: 3,
-              paddingHorizontal: 15,
-              //backgroundColor: 'pink',
-              borderRadius: 20,
-              color: 'white',
-              borderWidth: 1,
-              borderColor: 'gray',
-              fontWeight: 500,
+              position: 'relative',
             }}>
-            Edit profile
-          </Text>
-          <Text
-            style={{
-              padding: 3,
-              paddingHorizontal: 15,
-              //backgroundColor: 'pink',
-              borderRadius: 20,
-              color: 'white',
-              borderWidth: 1,
-              borderColor: 'gray',
-              fontWeight: 500,
-            }}>
-            Follow
-          </Text>
-          <Text
-            style={{
-              padding: 3,
-              paddingHorizontal: 15,
-              backgroundColor: 'rgba(255, 255, 255, 0.2)',
-              borderRadius: 20,
-              color: 'white',
-              borderWidth: 1,
-              borderColor: 'gray',
-              fontWeight: 500,
-            }}>
-            Unfollow
-          </Text>
-        </View>
+            {/* header */}
+            <View style={{position: 'relative'}}>
+              <TouchableOpacity
+                //activeOpacity={1}
+                onPress={() => {
+                  navigation.navigate('CoverImageUpdate');
+                }}>
+                <View
+                  style={{
+                    backgroundColor: 'gray',
+                    height: 150,
+                    width: screenWidth,
+                  }}></View>
+              </TouchableOpacity>
+              <View
+                style={{
+                  backgroundColor: '#0f1014',
+                  height: 80,
+                  width: 80,
+                  borderRadius: 50,
+                  position: 'absolute',
+                  bottom: -40,
+                  left: 30,
+                  padding: 3,
+                }}>
+                <TouchableOpacity
+                  //activeOpacity={1}
+                  onPress={() => {
+                    navigation.navigate('ProfileImageUpdate');
+                  }}>
+                  <Image
+                    source={require('../image/user.png')}
+                    style={{width: '100%', height: '100%'}}
+                  />
+                </TouchableOpacity>
+              </View>
 
-        <View style={{paddingHorizontal: 10}}>
-          <Text style={{color: 'white', fontSize: 20}}>Shikhar Mall</Text>
-          <Text style={{color: 'gray', fontSize: 16}}>@shikharrr24</Text>
+              <TouchableOpacity
+                onPress={togglePopUp}
+                activeOpacity={1}
+                style={{
+                  //backgroundColor: '#0f1014',
+                  //backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                  backgroundColor: 'rgba(255, 255, 255, 0.2)',
+                  position: 'absolute',
+                  top: 0,
+                  right: 0,
+                  padding: 7,
+                  borderRadius: 50,
+                  margin: 2,
+                }}>
+                <Svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20px"
+                  height="20px"
+                  fill="#ffffff"
+                  viewBox="0 0 52 52">
+                  <Path d="M20 44c0-3.3 2.7-6 6-6s6 2.7 6 6-2.7 6-6 6-6-2.7-6-6zm0-18c0-3.3 2.7-6 6-6s6 2.7 6 6-2.7 6-6 6-6-2.7-6-6zm0-18c0-3.3 2.7-6 6-6s6 2.7 6 6-2.7 6-6 6-6-2.7-6-6z" />
+                </Svg>
+              </TouchableOpacity>
 
-          <Text style={{color: 'white', fontSize: 16}}>
-            Passionate software engineer 🚀 | Transforming ideas into code 💻 |
-            Continuous learner & problem solver | Creator of digital solutions |
-            #TechEnthusiast | Coffee lover ☕️
-          </Text>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Svg
-              width="15px"
-              height="15px"
-              viewBox="0 0 16 16"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="#919191">
-              <Path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M14.5 2H13V1h-1v1H4V1H3v1H1.5l-.5.5v12l.5.5h13l.5-.5v-12l-.5-.5zM14 14H2V5h12v9zm0-10H2V3h12v1zM4 8H3v1h1V8zm-1 2h1v1H3v-1zm1 2H3v1h1v-1zm2-4h1v1H6V8zm1 2H6v1h1v-1zm-1 2h1v1H6v-1zm1-6H6v1h1V6zm2 2h1v1H9V8zm1 2H9v1h1v-1zm-1 2h1v1H9v-1zm1-6H9v1h1V6zm2 2h1v1h-1V8zm1 2h-1v1h1v-1zm-1-4h1v1h-1V6z"
-              />
-            </Svg>
-            <Text style={{color: 'gray', fontSize: 16, padding: 5}}>
-              Joined November 2023
-            </Text>
-          </View>
+              {isShow ? (
+                <View
+                  style={{
+                    backgroundColor: '#0f1014',
+                    position: 'absolute',
+                    top: 40,
+                    right: 25,
+                    padding: 7,
+                    width: 200,
+                    height: 100,
+                    borderRadius: 10,
+                  }}></View>
+              ) : null}
+            </View>
 
-          <Text style={{color: 'gray', fontSize: 16}}>
-            {' '}
-            <Text style={{color: 'white'}}>24</Text> Followers
-          </Text>
-        </View>
+            <View
+              style={{
+                //backgroundColor: 'red',
+                padding: 10,
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+              }}>
+              <Text
+                style={{
+                  padding: 3,
+                  paddingHorizontal: 15,
+                  //backgroundColor: 'pink',
+                  borderRadius: 20,
+                  color: 'white',
+                  borderWidth: 1,
+                  borderColor: 'gray',
+                  fontWeight: 500,
+                }}>
+                Edit profile
+              </Text>
+              <Text
+                style={{
+                  padding: 3,
+                  paddingHorizontal: 15,
+                  //backgroundColor: 'pink',
+                  borderRadius: 20,
+                  color: 'white',
+                  borderWidth: 1,
+                  borderColor: 'gray',
+                  fontWeight: 500,
+                }}>
+                Follow
+              </Text>
+              <Text
+                style={{
+                  padding: 3,
+                  paddingHorizontal: 15,
+                  backgroundColor: 'rgba(255, 255, 255, 0.2)',
+                  borderRadius: 20,
+                  color: 'white',
+                  borderWidth: 1,
+                  borderColor: 'gray',
+                  fontWeight: 500,
+                }}>
+                Unfollow
+              </Text>
+            </View>
 
-        {/*<View
+            <View style={{paddingHorizontal: 10}}>
+              <Text style={{color: 'white', fontSize: 20}}>Shikhar Mall</Text>
+              <Text style={{color: 'gray', fontSize: 16}}>@shikharrr24</Text>
+
+              <Text style={{color: 'white', fontSize: 16}}>
+                Passionate software engineer 🚀 | Transforming ideas into code
+                💻 | Continuous learner & problem solver | Creator of digital
+                solutions | #TechEnthusiast | Coffee lover ☕️
+              </Text>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Svg
+                  width="15px"
+                  height="15px"
+                  viewBox="0 0 16 16"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#919191">
+                  <Path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M14.5 2H13V1h-1v1H4V1H3v1H1.5l-.5.5v12l.5.5h13l.5-.5v-12l-.5-.5zM14 14H2V5h12v9zm0-10H2V3h12v1zM4 8H3v1h1V8zm-1 2h1v1H3v-1zm1 2H3v1h1v-1zm2-4h1v1H6V8zm1 2H6v1h1v-1zm-1 2h1v1H6v-1zm1-6H6v1h1V6zm2 2h1v1H9V8zm1 2H9v1h1v-1zm-1 2h1v1H9v-1zm1-6H9v1h1V6zm2 2h1v1h-1V8zm1 2h-1v1h1v-1zm-1-4h1v1h-1V6z"
+                  />
+                </Svg>
+                <Text style={{color: 'gray', fontSize: 16, padding: 5}}>
+                  Joined November 2023
+                </Text>
+              </View>
+
+              <Text style={{color: 'gray', fontSize: 16}}>
+                {' '}
+                <Text style={{color: 'white'}}>24</Text> Followers
+              </Text>
+            </View>
+
+            {/*<View
           style={{
             position: 'absolute',
             top: 0,
@@ -205,8 +223,12 @@ export default function MySpace({navigation}) {
             style={{width: screenWidth, height: 500}}
           />
         </View>*/}
-        <MySpaceTab />
-      </SafeAreaView>
-    </View>
+            <MySpaceTab />
+          </SafeAreaView>
+        </View>
+      ) : (
+        <MySpaceLogin />
+      )}
+    </>
   );
 }
